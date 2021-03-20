eb261aae57d13df25531174139c4e3d6dc51c287
Henrique Ramos <henriqueramos0106@gmail.com>
2021-03-20 16:14:25 -0300

    Desafio emails

---
+++ quantificadores/DesafioEmail.js
@@ -0,0 +1,13 @@
+const texto = `
+Os e-mails dos convidados são:
+  - fulano@cod3r.com.br
+  - xico@gmail.com
+  - joao@empresa.info.br
+  - maria_silva@registro.br
+  - rafa.sampaio@yahoo.com
+`;
+
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g));
+
+// no futuro..
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}(\.\w{2})?/g));
