f0d5e673763cbd4be9120982168f4766e662cf17
Henrique Ramos <henriqueramos0106@gmail.com>
2021-03-26 20:02:34 -0300

    Bordas

---
+++ bordas/Bordas.js
@@ -0,0 +1,18 @@
+const texto = 'Romário era um excelente jogador\n, mas hoje é um político questionador';
+
+console.log(texto.match(/r/gi));
+
+
+/**
+ * ^ (circunflexo) 
+ * Dentro de um conjunto representa um conjunto negado.
+ * Fora de um conjunto, ele representa um início de uma linha/string.
+ */
+
+console.log(texto.match(/^r/gi));
+
+// $ Representa o final de uma linha/string.
+
+console.log(texto.match(/r$/gi));
+
+console.log(texto.match(/^r.*r$/gi)); // Retorna null devido o problema do dotall no Javascript
